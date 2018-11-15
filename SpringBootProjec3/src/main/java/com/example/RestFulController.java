965e4ff3aa578c2e0776b2338f930ec4d3cce013
Yosaporn <yosaporn.pe@gmail.com>
2018-11-15 13:35:08 +0700

    เพิ่ม method post ที่รับ param

--- SpringBootProjec3/src/main/java/com/example/RestFulController.java
+++ SpringBootProjec3/src/main/java/com/example/RestFulController.java
@@ -1,24 +1,41 @@
-
 package com.example;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/index")
+@RequestMapping(path = "/index", 
+        consumes = MediaType.APPLICATION_JSON_VALUE, 
+        produces = MediaType.APPLICATION_JSON_VALUE, 
+        method = {RequestMethod.GET, RequestMethod.POST})
 public class RestFulController {
-    
-	@RequestMapping(value = "/ex/foos", method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
-	public String hello() {
-		return "Howdy " + ", have a good day";
-	}
 
-	@RequestMapping("/get/{name}")
-	public String get(@PathVariable String name) {
-		return "Howdy " + name + ", have a good day";
-	}
+    //Get method
+    @RequestMapping(value = "/get/{name}", method = RequestMethod.GET)
+    public String helloGet(@PathVariable String name) {
+        return "Hello " + name + ", have a good day";
+    }
+
+    //Post method
+    @RequestMapping(value = "/post/noParam", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost() {
+        return "have a good day";
+    }
+    
+    //Post method (param)
+    @RequestMapping(value = "/post/withParam", method = RequestMethod.POST)
+    @ResponseBody
+    public ObjectRequest helloPostReq(@RequestBody ObjectRequest request) {
+        ObjectRequest objectRequest = new ObjectRequest();
+        objectRequest.setName(request.getName()+" Response");
+        objectRequest.setValue(request.getValue()+" Response");
+        
+        return objectRequest;
+    }
 }
