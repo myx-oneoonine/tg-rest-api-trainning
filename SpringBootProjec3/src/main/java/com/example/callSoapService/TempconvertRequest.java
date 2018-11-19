40cc5aa67920011fde104c1cb154d4860a4683f9
Yosaporn <yosaporn.pe@gmail.com>
2018-11-19 15:32:53 +0700

    validate min number and fix msg

--- SpringBootProjec3/src/main/java/com/example/callSoapService/TempconvertRequest.java
+++ SpringBootProjec3/src/main/java/com/example/callSoapService/TempconvertRequest.java
@@ -1,10 +1,12 @@
 package com.example.callSoapService;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 public class TempconvertRequest {
 
     @NotNull
+    @Min(value=1, message="Please input more than 0")
     private String celsius;
 
     public String getCelsius() {
