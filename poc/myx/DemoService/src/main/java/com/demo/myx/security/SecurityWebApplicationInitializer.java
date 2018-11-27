8d23255004077e0ef7bfe332e00a1a431dd880bb
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-11-27 22:29:40 +0700

    demo: remove useless class

--- poc/myx/DemoService/src/main/java/com/demo/myx/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.demo.myx.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
