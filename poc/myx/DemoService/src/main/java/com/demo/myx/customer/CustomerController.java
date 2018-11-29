9b86727bee33f6ed3fe429b6796d3336b4e658b9
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-11-29 22:59:24 +0700

    demo: ใส่ context /api ให้ไม่สับสน

--- poc/myx/DemoService/src/main/java/com/demo/myx/customer/CustomerController.java
+++ poc/myx/DemoService/src/main/java/com/demo/myx/customer/CustomerController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api/customers")
 public class CustomerController {
 
 	@Autowired
