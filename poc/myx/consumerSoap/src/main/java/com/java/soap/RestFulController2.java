23fc3f1990a7e7b15aa51942bc7086ed55d24f4f
Yosaporn <yosaporn.pe@gmail.com>
2018-12-07 13:26:25 +0700

    แก้ code แยกฟังก์ชัน marshall && unmarshall

--- poc/myx/consumerSoap/src/main/java/com/java/soap/RestFulController2.java
+++ poc/myx/consumerSoap/src/main/java/com/java/soap/RestFulController2.java
@@ -3,6 +3,7 @@ package com.java.soap;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
@@ -10,6 +11,7 @@ import java.util.logging.Logger;
 import javax.validation.Valid;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.soap.MessageFactory;
 import javax.xml.soap.SOAPConnection;
@@ -25,38 +27,53 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/callSoapService")
 public class RestFulController2 {
 
-    //POST method
-    @PostMapping(value = "/temp")
-    public TempconvertResponse processRequest(@Valid @RequestBody TempconvertRequest req) throws MalformedURLException, IOException {
-        
-        try {
-            SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = connectionFactory.createConnection();
-            URL urlWsdl = new URL("https://www.w3schools.com/xml/tempconvert.asmx?wsdl");
-            String mesg = "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
-                    + "<soap:Body>"
-                    + "<CelsiusToFahrenheit xmlns=\"https://www.w3schools.com/xml/\">"
-                    + "<Celsius>" + req.getCelsius() + "</Celsius>"
-                    + "</CelsiusToFahrenheit>"
-                    + "</soap:Body>"
-                    + "</soap:Envelope>";
+	// POST method
+	@PostMapping(value = "/temp")
+	public Class<?> processRequest(@Valid @RequestBody TempconvertRequest req)
+			throws MalformedURLException, IOException {
 
-            InputStream inputStream = new ByteArrayInputStream(mesg.getBytes());
-            SOAPMessage soapRequest = MessageFactory.newInstance().createMessage(null, inputStream);
-            SOAPMessage soapResponse = soapConnection.call(soapRequest, urlWsdl);
-//            soapResponse.writeTo(System.out);
-            JAXBContext jaxbContext = JAXBContext.newInstance(TempconvertResponse.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            
-            TempconvertResponse tempconvertResponse = (TempconvertResponse) unmarshaller.unmarshal(soapResponse.getSOAPBody().extractContentAsDocument());
+		try {
+			SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
+			SOAPConnection soapConnection = connectionFactory.createConnection();
+			URL urlWsdl = new URL("https://www.w3schools.com/xml/tempconvert.asmx?wsdl");
+			String mesg = "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+					+ "<soap:Body>" + "<CelsiusToFahrenheit xmlns=\"https://www.w3schools.com/xml/\">" + "<Celsius>"
+					+ req.getCelsius() + "</Celsius>" + "</CelsiusToFahrenheit>" + "</soap:Body>" + "</soap:Envelope>";
 
-            return tempconvertResponse;
-        } catch (IOException | UnsupportedOperationException | SOAPException exception) {
-            System.out.println(exception.getMessage());
-            return null;
-        } catch (JAXBException exception) {
-            Logger.getLogger(RestFulController2.class.getName()).log(Level.SEVERE, null, exception);
-            return null;
-        }
-    }
+			InputStream inputStream = new ByteArrayInputStream(mesg.getBytes());
+			SOAPMessage soapMessage = MessageFactory.newInstance().createMessage(null, inputStream);
+			
+			soapMessage = soapConnection.call(soapMessage, urlWsdl);
+
+			Class<?> tempconvertResponse = convertXMLToObject(TempconvertResponse.class, soapMessage);
+			System.out.println(convertObjectToXML(TempconvertResponse.class, tempconvertResponse));
+
+			return tempconvertResponse;
+		} catch (IOException | UnsupportedOperationException | SOAPException exception) {
+			System.out.println(exception.getMessage());
+			return null;
+		} catch (JAXBException exception) {
+			Logger.getLogger(RestFulController2.class.getName()).log(Level.SEVERE, null, exception);
+			return null;
+		}
+	}
+
+	private String convertObjectToXML(Class<?> class1, Object object) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(class1);
+		StringWriter sw = new StringWriter();
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.marshal(object, sw);
+		String xmlString = sw.toString();
+		
+		return xmlString;
+	}
+	
+	private Class<?> convertXMLToObject(Class<?> class1, SOAPMessage soapMessage) throws JAXBException, SOAPException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(class1);
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+		Class<?> tempconvertResponse = (Class<?>) unmarshaller.unmarshal(soapMessage.getSOAPBody().extractContentAsDocument());
+		
+		return tempconvertResponse;
+	}
 }
