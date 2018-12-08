1829c95aeee7e993d7048589035fb1422effd037
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-12-08 20:32:08 +0700

    authorize: new authrize server

---
+++ poc/myx/security/authorization-server/src/main/java/org/demo/config/AuthorizationServerConfigurer.java
@@ -0,0 +1,43 @@
+package org.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfigurer extends AuthorizationServerConfigurerAdapter {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+		.withClient("clientID").secret("{noop}clientSecret")
+			.accessTokenValiditySeconds(120) 
+			.refreshTokenValiditySeconds(86400) 
+		.scopes("read", "write") 
+			.authorizedGrantTypes("password", "refresh_token")
+		.and().withClient("clientID2").secret("{noop}clientSecret")
+			.accessTokenValiditySeconds(120) 
+			.refreshTokenValiditySeconds(86400) 
+		.scopes("read", "write") 
+			.authorizedGrantTypes("password", "refresh_token");
+	}
+	
+	@Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
