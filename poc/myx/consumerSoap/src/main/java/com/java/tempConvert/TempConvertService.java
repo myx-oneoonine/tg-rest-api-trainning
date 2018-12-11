025d9609bd789c8ac678e9598f62247114287a2d
Yosaporn <yosaporn.pe@gmail.com>
2018-12-11 17:07:39 +0700

    no message

--- poc/myx/consumerSoap/src/main/java/com/java/tempConvert/TempConvertService.java
+++ poc/myx/consumerSoap/src/main/java/com/java/tempConvert/TempConvertService.java
@@ -11,6 +11,7 @@ import javax.xml.soap.SOAPConnection;
 import javax.xml.soap.SOAPConnectionFactory;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.soap.SOAPPart;
 
@@ -19,50 +20,49 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping(path = "/service")
 public class TempConvertService {
-	
+
 	@PostMapping(value = "/convertCelsiusToFarenheit")
-	public TempconvertResponse convertCelsiusToFarenheit(@RequestBody TempconvertRequest request) {
+	public TempconvertResponse convertCelsiusToFarenheit(@RequestBody TempconvertRequest request)
+			throws UnsupportedOperationException, SOAPException {
 		TempconvertResponse tempconvertResponse = new TempconvertResponse();
-        try {
-            SOAPConnectionFactory connectionFactory = SOAPConnectionFactory.newInstance();
-            SOAPConnection soapConnection = connectionFactory.createConnection();
-            URL urlWsdl = new URL("https://www.w3schools.com/xml/tempconvert.asmx?wsdl");
-        	MessageFactory messageFactory = MessageFactory.newInstance();
-        	SOAPMessage soapMessage = messageFactory.createMessage();
-        	SOAPPart soapPart = soapMessage.getSOAPPart();
+		SOAPConnection soapConnection = SOAPConnectionFactory.newInstance().createConnection();
+		SOAPMessage soapMessage = MessageFactory.newInstance().createMessage();
 
-            // SOAP Envelope
-            SOAPEnvelope envelope = soapPart.getEnvelope();
-            envelope.addNamespaceDeclaration("xsd", "http://www.w3.org/2001/XMLSchema");
-            envelope.addNamespaceDeclaration("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-            envelope.addNamespaceDeclaration("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+		try {
+			URL urlWsdl = new URL("https://www.w3schools.com/xml/tempconvert.asmx?wsdl");
+			SOAPPart soapPart = soapMessage.getSOAPPart();
 
-            // SOAP Body
-            SOAPBody soapBody = envelope.getBody();
-            SOAPElement soapBodyElem = soapBody.addChildElement("CelsiusToFahrenheit", "", "https://www.w3schools.com/xml/");
-            SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("Celsius");
-            soapBodyElem1.addTextNode(request.getCelsius());
-            
-            // SOAP Action
-            MimeHeaders headers = soapMessage.getMimeHeaders();
-            headers.addHeader("SOAPAction", "https://www.w3schools.com/xml/CelsiusToFahrenheit");
-            
-            soapMessage.saveChanges();
-            
-            SOAPMessage soapResponse = soapConnection.call(soapMessage, urlWsdl);
+			// SOAP Envelope
+			SOAPEnvelope envelope = soapPart.getEnvelope();
+			envelope.addNamespaceDeclaration("xsd", "http://www.w3.org/2001/XMLSchema");
+			envelope.addNamespaceDeclaration("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+			envelope.addNamespaceDeclaration("soap", "http://schemas.xmlsoap.org/soap/envelope/");
 
-            JAXBContext jaxbContext = JAXBContext.newInstance(TempconvertResponse.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            
-            tempconvertResponse = (TempconvertResponse) unmarshaller.unmarshal(soapResponse.getSOAPBody().extractContentAsDocument());
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
-        return tempconvertResponse;
-    }
+			// SOAP Body
+			SOAPBody soapBody = envelope.getBody();
+			SOAPElement soapBodyElem = soapBody.addChildElement("CelsiusToFahrenheit", "", "https://www.w3schools.com/xml/");
+			SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("Celsius");
+			soapBodyElem1.addTextNode(request.getCelsius());
+
+			// SOAP Action
+			MimeHeaders headers = soapMessage.getMimeHeaders();
+			headers.addHeader("SOAPAction", "https://www.w3schools.com/xml/CelsiusToFahrenheit");
+
+			SOAPMessage soapResponse = soapConnection.call(soapMessage, urlWsdl);
+
+			JAXBContext jaxbContext = JAXBContext.newInstance(TempconvertResponse.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+			tempconvertResponse = (TempconvertResponse) unmarshaller.unmarshal(soapResponse.getSOAPBody().extractContentAsDocument());
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		} finally {
+			soapConnection.close();
+		}
+		return tempconvertResponse;
+	}
 
 }
