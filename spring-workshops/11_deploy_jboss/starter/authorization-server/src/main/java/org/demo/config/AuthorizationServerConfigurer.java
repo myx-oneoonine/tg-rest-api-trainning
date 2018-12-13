7bab3d486ef2937ca77e7c8b71ebfc649870647a
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-12-13 21:48:48 +0700

    workshop_11  add starter project

---
+++ spring-workshops/11_deploy_jboss/starter/authorization-server/src/main/java/org/demo/config/AuthorizationServerConfigurer.java
@@ -0,0 +1,63 @@
+package org.demo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfigurer extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private TokenStore tokenStore;
+	
+	@Bean
+	public JdbcTokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//		clients.inMemory()
+//		.withClient("clientID")
+//			.secret("{noop}clientSecret")
+//			.accessTokenValiditySeconds(120)
+//			.refreshTokenValiditySeconds(86400)
+//			.scopes("read", "write")
+//			.authorizedGrantTypes("password", "refresh_token")
+//		.and().withClient("clientID2")
+//			.secret("{noop}clientSecret")
+//			.accessTokenValiditySeconds(120)
+//			.refreshTokenValiditySeconds(86400)
+//			.scopes("read", "write")
+//			.authorizedGrantTypes("password", "refresh_token");
+		clients.jdbc(dataSource);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//		endpoints.authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
+	}
+}
